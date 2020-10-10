6e4ddd4876e5d8a89044e8b36415c59bd00d4836
quidvil <gurprit.iiest@gmail.com>
2020-10-10 13:06:52 +0530

    changed global styles

--- crownFashion-master/client/src/global.styles.js
+++ crownFashion-master/client/src/global.styles.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
 body{
     font-family: 'Open Sans Condensed',sans-serif;
-    padding: 20px 60px;
+    padding: 20px 30px;
 
     @media screen and (max-width: 800px){
         padding: 10px;

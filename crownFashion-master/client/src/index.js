1047d7883c2f7b77e6def5af41b2ff1fae259c24
quidvil <gurprit.iiest@gmail.com>
2020-10-10 12:57:58 +0530

    made app pwa compliant, added service-worker, updated manifest, added enforce to redirect http to https

--- crownFashion-master/client/src/index.js
+++ crownFashion-master/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import * as serviceWorker from './serviceWorker.js';
 import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
@@ -24,3 +24,4 @@ ReactDOM.render(
 , 
 document.getElementById('root'));
 
+serviceWorker.register();
